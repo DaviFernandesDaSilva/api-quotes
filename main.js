029bd3a5cb21a1c62dff1872df65613df6f3c611
Davipbr15 <davipbr90@gmail.com>
2022-05-07 12:11:41 -0300

    vai pf

--- main.js
+++ main.js
@@ -1,38 +1,30 @@
 const express = require ('express');
 const app = express();
+const cors = require("cors");
 
-  
-// Defining get request at '/' route
-app.get('/', function(req, res) {
-    res.json({
-      number: 1
-    });
-  });
-    
-  // Defining get request at '/multiple' route
-  app.get('/multiple', function(req, res) {
-    res.json({
-      number: 1,
-      name: 'John',
-      gender: 'male'
-    });
-  });
-    
-  // Defining get request at '/array' route
-  app.get('/array', function(req, res) {
-    res.json([{
-        number: 1,
-        name: 'John',
-        gender: 'male'
-      },
-      {
-        number: 2,
-        name: 'Ashley',
-        gender: 'female'
-      }
-    ]);
-  });
-    
+app.use(cors());
+app.use(express.json());
+
+var fs = require("fs");
+var jsonData;
+
+fs.readFile("frases.json" , "utf8", function(err, data){
+  if(err){
+    return console.log("Erro ao ler arquivo");
+  }
+
+  jsonData = JSON.parse(data); // faz o parse para json
+
+})
+
+app.get('/', (req, res) => { res.json(jsonData) });
+
+app.get('/api/v1/allFrases', (req, res) =>
+    res.json(
+    jsonData
+    ));
+
+
   // Setting the server to listen at port 3000
   app.listen(3000, function(req, res) {
     console.log("Server is running at port 3000");
