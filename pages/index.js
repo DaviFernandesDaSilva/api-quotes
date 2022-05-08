6fbdad5ed3427914a4667f1841a3ca63ed3a6852
Davipbr15 <davipbr90@gmail.com>
2022-05-07 23:36:26 -0300

    Tailwind Implementado

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
 
   return (
     <div className="bg-red-500">
-      <h1>Citação</h1>
+      <h1 className="font-bold">Citação</h1>
       <h3>{citA}</h3>
       <h2>{autoR}</h2>
       
