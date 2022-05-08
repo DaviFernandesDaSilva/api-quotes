40a06511703904df3f21b63715e4264d36f671fa
Davipbr15 <davipbr90@gmail.com>
2022-05-08 09:02:02 -0300

    a

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import Header from '../components/Header.js';
 import Footer from '../components/Footer.js';
 
+
 export default function Home() {
   var i,autor,cita;
 
@@ -48,7 +49,7 @@ export default function Home() {
               </p>
             <button type="button" 
             value="Refresh Page"
-            className="scale-90 inline-block px-6 py-2.5 bg-gray-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-500 hover:shadow-lg focus:bg-gray-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out"
+            className="inline-block px-6 py-2.5 bg-gray-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-500 hover:shadow-lg focus:bg-gray-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out"
             >Gerar outra citação</button>
           </div>
         </div>
